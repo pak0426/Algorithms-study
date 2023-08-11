0946bdc17809a3c47447c7943371edf4021654a8
hmmini <59166263+pak0426@users.noreply.github.com>
2023-08-11 23:17:46 +0900

    [Bronze V] Title: 구구단, Time: 256 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+        in.close();
+
+        for (int i=1; i < 10; i++) {
+            System.out.println(num + " * " + i + " = " + num*i);
+        }
+    }
+}
