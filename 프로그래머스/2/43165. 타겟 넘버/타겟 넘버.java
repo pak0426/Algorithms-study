f4c73ce9c831417a73788fda5b47a7b9200cf6dc
hmmini <59166263+pak0426@users.noreply.github.com>
2024-03-20 00:02:13 +0900

    [level 2] Title: 타겟 넘버, Time: 0.69 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    private int answer = 0;
+    private final int depth = 0;
+    private final int sum = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, depth, target, sum);
+        return answer;
+    }
+    
+    public void dfs(int[] numbers, int depth, int target, int sum) {
+        if (depth == numbers.length) {
+            if (target == sum) {
+                answer++;
+            }
+            return;
+        }
+        dfs(numbers, depth + 1, target, sum + numbers[depth]);
+        dfs(numbers, depth + 1, target, sum - numbers[depth]);
+    }
+}
