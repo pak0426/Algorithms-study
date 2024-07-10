e31cb2d7f989468a1933d007209dc90c3fbca6f1
hmmini <59166263+pak0426@users.noreply.github.com>
2024-07-10 17:06:24 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.29 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -4,19 +4,16 @@ import java.util.*;
 // 행과 열의 길이는 100이하 = 시간 복잡도를 신경쓰지 않아도 된다.
 // 곱할 수 있는 배열만 주어진다 == 예외 처리를 신경쓰지 않아도 된다.
 // 행렬의 곱은? 요소들을 곱한거에 대한 더하기임
-// ex) [[1,4]] [[3,3]] = (1 * 3) + (4 * 3) = 15
-// arr1[0][0] * arr2[0][0] + arr[0][1] * arr2[0][1]
-// arr1[1][0] * arr2[0][0]
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {        
-        // 행과 열을 정의
-        int r1 = arr1.length;
-        int c1 = arr1[0].length;
-        int r2 = arr2.length;
-        int c2 = arr2[0].length;
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
         
-        // 정답 배열 초기화
-        int[][] answer = new int[r1][c2];
+        int r1 = arr1.length; // 3
+        int c1 = arr1[0].length; // 2
+        int r2 = arr2.length; // 2
+        int c2 = arr2[0].length; // 2
+        
+        answer = new int[r1][c2];
         
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c2; j++) {
@@ -25,6 +22,7 @@ class Solution {
                 }
             }
         }
+        
         
         return answer;
     }
