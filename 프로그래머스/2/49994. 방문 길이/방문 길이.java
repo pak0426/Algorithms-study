722d02a64c02769f6b2252fd9db223fdab4f52ce
hmmini <59166263+pak0426@users.noreply.github.com>
2024-07-11 16:17:55 +0900

    [level 2] Title: 방문 길이, Time: 15.48 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -0,0 +1,77 @@
+/*
+문제 파악
+- dirs는 String, 500이하의 자연수 길이
+
+- 원점을 5,5로 생각하자
+*/
+
+import java.util.*;
+
+class Solution {
+    private int x = 5;
+    private int y = 5;
+    
+    public int solution(String dirs) {
+        int answer = 0;        
+        
+        Set<String> set = new HashSet<>();
+        
+        int nx = 0;
+        int ny = 0;
+        for (int i = 0; i < dirs.length(); i++) {      
+            
+            String before = x + " " + y;
+            if (dirs.charAt(i) == 'U') {
+                if (!up()) continue;
+            } else if (dirs.charAt(i) == 'D') {
+                if (!down()) continue;
+            } else if (dirs.charAt(i) == 'L') {
+                if (!left()) continue;
+            } else {
+                if (!right()) continue;
+            }
+            nx = x;
+            ny = y;
+            
+            String after = nx + " " + ny;
+            set.add(before + "^" + after);
+            set.add(after + "^" + before);
+        }
+        System.out.println(x + ", " + y);
+        System.out.println(set);
+        
+        return set.size() / 2;
+    }
+    
+    public boolean left() {
+        if (x <= 0) {
+            return false;
+        }
+        x -= 1;
+        return true;
+    }
+    
+    public boolean right() {
+        if (x >= 10) {
+            return false;
+        }
+        x += 1;
+        return true;
+    }
+    
+    public boolean up() {
+        if (y >= 10) {
+            return false;
+        }
+        y += 1;
+        return true;
+    }
+    
+    public boolean down() {
+        if (y <= 0) {
+            return false;
+        }
+        y -= 1;
+        return true;
+    }
+}
