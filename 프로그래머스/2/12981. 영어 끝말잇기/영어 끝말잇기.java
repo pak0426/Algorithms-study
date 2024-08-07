d294974948ca1ae40568a375cbb730db0e1c14ba
hmmini <59166263+pak0426@users.noreply.github.com>
2024-08-07 13:07:44 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.12 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,39 @@
+/*
+문제 파악
+- 1부터 n까지 붙어있는 n명의 사람이 영어 끝말잇기를 한다.
+
+- 사람의 수 n, 말한 단어 배열 words
+- 가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에서 탈락하는지를 구해서 return
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+
+        int loop = 0;
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < words.length; i++) {
+            if (i % n == 0) {
+                loop++;
+            }
+            
+            String word = words[i];
+            if (i > 0) {
+                String bfWord = words[i - 1];
+                
+                if (!word.startsWith(bfWord.substring(bfWord.length() - 1))) {
+                    return new int[]{i % n + 1, loop};
+                }
+            }
+            
+            if (set.contains(words[i])) {
+                return new int[]{i % n + 1, loop};
+            }
+            set.add(words[i]);
+        }
+
+        return answer;
+    }
+}
