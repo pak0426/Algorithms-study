63b8c390f3aa33349a31508af6c98dcfb7a7c123
hmmini <59166263+pak0426@users.noreply.github.com>
2024-08-11 10:02:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.27 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,62 +1,72 @@
-import java.util.ArrayDeque;
+/*
+문제 파악
+- ROR 게임은 두 팀으로 나누어서 진행하며, 상태 팀 진영을 먼저 파괴하면 이기는 게임이다. 
+- 따라서, 각 팀은 상대 팀 진영에 최대한 빨리 도착하는 것이 유리하다.
 
-public class Solution {
+- 지나가는 칸의 최솟값을 구해야함.
+- 최적값, 최솟값을 구하는건 너비 우선 탐색이다. (BFS)
 
-    // ❶ 이동할 수 있는 방향을 나타내는 배열 rx, ry 선언
+*/
+
+import java.util.*;
+
+class Solution {
+    
+    // 이동할 수 있는 방향을 나타내는 배열
     private static final int[] rx = {0, 0, 1, -1};
     private static final int[] ry = {1, -1, 0, 0};
-
+    
     private static class Node {
-        int r, c;
-
+        int r; // row
+        int c; // cell
+        
         public Node(int r, int c) {
             this.r = r;
             this.c = c;
         }
     }
-
+    
     public int solution(int[][] maps) {
-        // ❷ 맵의 크기를 저장하는 변수 선언
+        
         int N = maps.length;
         int M = maps[0].length;
-
-        // ❸ 최단 거리를 저장할 배열 생성
-        int[][] dist = new int[N][M];
-
-        // ❹ bfs 탐색을 위한 큐 생성
+        
+        // 최단 거리를 저장
+        int[][] distance = new int[N][M];
+        
         ArrayDeque<Node> queue = new ArrayDeque<>();
-
-        // ❺ 시작 정점에 대해서 큐에 추가, 최단 거리 저장
+        
         queue.addLast(new Node(0, 0));
-        dist[0][0] = 1;
-
-        // ❻ queue가 빌 때까지 반복
+        distance[0][0] = 1;
+        
         while (!queue.isEmpty()) {
             Node now = queue.pollFirst();
-
-            // ❼ 현재 위치에서 이동할 수 있는 모든 방향
+            
+            // now에서 이동할 수 있는 방향
             for (int i = 0; i < 4; i++) {
                 int nr = now.r + rx[i];
                 int nc = now.c + ry[i];
-
-                // ❽ 맵 밖으로 나가는 경우 예외처리
-                if (nr < 0 || nc < 0 || nr >= N || nc >= M)
+                
+                // 맵 밖으로 나가는 경우 예외처리
+                if (nr < 0 || nc < 0 || nr >= N || nc >= M) {
                     continue;
-
-                // ❾ 벽으로 가는 경우 예외처리
-                if (maps[nr][nc] == 0)
+                }
+                
+                // 방해물 벽으로 가는 경우 예외처리
+                if (maps[nr][nc] == 0) {
                     continue;
-
-                // ➓ 이동한 위치가 처음 방문하는 경우, queue에 추가하고 거리 갱신
-                if (dist[nr][nc] == 0) {
+                }
+                
+                // 이동한 위치가 처음 방문이라면, queue에 추가하고 거리 갱신
+                if (distance[nr][nc] == 0) {
                     queue.addLast(new Node(nr, nc));
-                    dist[nr][nc] = dist[now.r][now.c] + 1;
+                    distance[nr][nc] = distance[now.r][now.c] + 1;
                 }
             }
         }
-
-        // 목적지까지 최단 거리 반환, 목적지에 도달하지 못한 경우에는 -1 반환
-        return dist[N - 1][M - 1] == 0 ? -1 : dist[N - 1][M - 1];
+        
+        return distance[N - 1][M - 1] == 0 ? -1 : distance[N - 1][M - 1];
     }
-
+    
+    
 }
