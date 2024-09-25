2b18fb81c27a490cf96cf74b2ca536651556e07e
hmmini <59166263+pak0426@users.noreply.github.com>
2024-09-25 15:45:50 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.83 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,17 +1,17 @@
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Solution {
 
     public long solution(long n) {
-        String[] digits = String.valueOf(n).split("");
-
-        Arrays.sort(digits, Collections.reverseOrder());
-
+        String str = String.valueOf(n);
+        String[] arr = str.split("");
+        Arrays.sort(arr, Collections.reverseOrder());
+        
         StringBuilder sb = new StringBuilder();
-        for (String digit : digits)
-            sb.append(digit);
-
+        for (String s : arr) {
+            sb.append(s);
+        }
+        
         return Long.parseLong(sb.toString());
     }
 
