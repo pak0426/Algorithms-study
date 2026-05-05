52a241c2f511954eabcf83da4a36c15228ca6bb9
pak0426 <59166263+pak0426@users.noreply.github.com>
2026-05-05 17:10:41 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,28 +1,25 @@
-/*
-문제 파악
-- 매일 다른 옷을 조합
-- 종류별로 최대 1가지 의상만 착용
-- 의상이 일부가 겹쳐도, 다른 의상이 겹치지 않거나, 의상을 추가로 더 착용한 경우에는 서로 다른 방법으로 착용한 것으로 계산
-- 코니는 하루에 최소 1개의 의상을 입는다.
-
-- 코니가 가진 의상들이 담긴 clothes가 주어질 때 서로 다른 옷의 조합의 수를 return
-*/
-
 import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            String category = clothes[i][1];
-            map.put(category, map.getOrDefault(category, 0) + 1);
+        
+        
+        for (String[] arr : clothes) {
+            String category = arr[1];
+            
+            int v = map.getOrDefault(category, 0);
+            map.put(category, v + 1);
         }
         
-        int answer = 1;
-        for (Integer value : map.values()) {
-            answer *= value + 1;
+        int result = 1;
+        
+        for (int v : map.values()) {
+            result *= (v + 1);
         }
         
-        return answer - 1;
+        result -= 1;
+        
+        return result;
     }
 }
