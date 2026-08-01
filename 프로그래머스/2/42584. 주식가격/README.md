f534fcfc786809edfa4ce0893eb36db19b45b10a
pak0426 <59166263+pak0426@users.noreply.github.com>
2026-08-01 15:38:14 +0900

    [level 2] Title: 주식가격, Time: 21.91 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?gad_source=1&gad_campaignid=23716289893) 
 
 ### 성능 요약
 
-메모리: 70.9 MB, 시간: 34.55 ms
+메모리: 75.5 MB, 시간: 21.91 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 16일 22:33:24
+2026년 08월 01일 15:38:11
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,34 +1,32 @@
-/*
-문제파악
-- 주식가격이 담긴 배열 prices
-- 가격이 떨어지지 않은 기간은 몇 초인지 return
-- 1 이상 10,000 이하의 자연수, 길이는 2 이상 100,000 이하
-  - 시간 복잡도를 고려해야함.
-*/
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
+        int[] answer = {};
         
-        Stack<Integer> stack = new Stack<>();
-        Stack<Integer> result = new Stack<>();
+        List<Integer> list = new ArrayList<>();
         
-        stack.push(0);
-        for (int i = 1; i < prices.length; i++) {
+        for (int i = 0; i < prices.length; i++) {
+            int now = prices[i];
+            int sec = 0;
             
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int j = stack.pop();
-                answer[j] = i - j;
+            for (int j = i + 1; j < prices.length; j++) {
+                int next = prices[j];
+                
+                if (next >= now) {
+                    sec++;
+                } else {
+                    sec++;
+                    break;
+                }
             }
-            stack.push(i);
+            
+            list.add(sec);
         }
         
-        while (!stack.isEmpty()) {
-            int j = stack.pop();
-            answer[j] = n - 1 - j;
-        }
+        answer = list.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
         
         return answer;
     }
